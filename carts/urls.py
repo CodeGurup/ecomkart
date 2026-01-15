786bdf4ec89eb8f572bd06adaaec1322a665b193
Peter <ngugipeter8922gmail.com>
2026-01-15 10:45:39 +0300

    store and cast functionality

--- carts/urls.py
+++ carts/urls.py
@@ -6,7 +6,7 @@ from . import views
 urlpatterns = [
     path( '', views.cart, name='cart' ) ,
     path( 'add/<int:product_id>/', views.add_cart, name='add_cart' ) ,
-    path( 'remove/<int:product_id>/', views.remove_cart, name='remove_cart' ) ,
-    path( 'remove_item/<int:product_id>/', views.remove_cart_item, name='remove_cart_item' ) ,
+    path( 'remove/<int:cart_item_id>/', views.remove_cart, name='remove_cart' ) ,
+    path( 'remove_item/<int:cart_item_id>/', views.remove_cart_item, name='remove_cart_item' ) ,
 ]
     
